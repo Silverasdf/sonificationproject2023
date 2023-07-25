f01eca6e2599f757d734aa0dc4f91a979910b9ff
Silverasdf <yhg461@vols.utk.edu>
2023-07-25 09:02:24 -0400

    Used enumeration for add_spectrograms.py

--- sonify/add_spectrograms.py
+++ sonify/add_spectrograms.py
@@ -1,4 +1,4 @@
-# Add spectrograms - Takes two random files from directory of .mat files, adds them together, and makes the spectrograms
+# Add spectrograms - Takes two files from directory of .mat files, adds them together, and makes the spectrograms
 # Ryan Peruski, 07/24/23
 import numpy as np
 import scipy.io.wavfile as wavfile
@@ -9,27 +9,29 @@ import os
 from sonifyfn import sonify
 
 data_dir = '/root/SonificationProject/Data/SomeMatFiles'
-picture_out = '/root/SonificationProject/Data/sonified_spectrograms_on'
-sound_out = '/root/SonificationProject/Data/sonified_spectrograms_on'
+picture_out = '/root/SonificationProject/Data/sonified_spectrograms_off'
+sound_out = '/root/SonificationProject/Data/sonified_spectrograms_off'
 num_files = 100
 
 #Choose random file from directory
-for i in range(num_files):
-    eligible_files = [file for file in os.listdir(data_dir) if file.endswith('1.mat')]
-    file = np.random.choice(eligible_files)
-    filepath = os.path.join(data_dir, file).replace('\\', '/')
+eligible_files = [file for file in os.listdir(data_dir) if file.endswith('2.mat')]
 
-    file2 = np.random.choice(eligible_files)
-    filepath2 = os.path.join(data_dir, file).replace('\\', '/')
+num = 0
+for file in eligible_files:
+    for file2 in eligible_files:
+        num += 1
+        print(f'Processing file {num} of {len(eligible_files)**2}')
+        filepath = os.path.join(data_dir, file).replace('\\', '/')
+        filepath2 = os.path.join(data_dir, file).replace('\\', '/')
 
-    sample = sio.loadmat(filepath)
-    sample2 = sio.loadmat(filepath2)
-    TheData = 0.5 * (sample['data'].astype(float) + sample2['data'].astype(float))
-    fs = sample['samp_rate'].item()
-    fs = float(fs)
-    # Basically the absolute path of the file (minus the drive name) adds that to your current directory. Makes all dirs needed
-    pOut = os.path.join(picture_out, file[:-4] + "_" + file2[:-4] + '.png')
-    sOut = os.path.join(sound_out, file[:-4] + "_" + file2[:-4] + '.wav')
-    sonify(TheData, fs, sOut, pOut, N=5)
+        sample = sio.loadmat(filepath)
+        sample2 = sio.loadmat(filepath2)
+        TheData = 0.5 * (sample['data'].astype(float) + sample2['data'].astype(float))
+        fs = sample['samp_rate'].item()
+        fs = float(fs)
+        # Basically the absolute path of the file (minus the drive name) adds that to your current directory. Makes all dirs needed
+        pOut = os.path.join(picture_out, file[:-4] + "_" + file2[:-4] + '.png')
+        sOut = os.path.join(sound_out, file[:-4] + "_" + file2[:-4] + '.wav')
+        sonify(TheData, fs, sOut, pOut, N=5)
 
 
